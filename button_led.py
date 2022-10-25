398fe3d83e0de5b059882bf9e4ad17490970aaf4
Wahni Adnani (DEF) <75670541+defrein@users.noreply.github.com>
2022-10-26 00:21:47 +0700

    Update button_led.py

--- button_led.py
+++ button_led.py
@@ -1,14 +1,20 @@
 # from signal import pause
-from gpiozero import LED, Button
+from gpiozero import LED, Button, DistanceSensor
 import time
 
 button = Button(12)
 ledGreen = LED(13)
 ledYellow = LED(26)
 ledRed = LED(19)
+sensor = DistanceSensor(echo=21, trigger=20)
 
 
 def on_led(ledColor, on_time):
     ledColor.on()
     time.sleep(on_time)
     ledColor.off()
+
+def calculateDistance():
+    objectDis = sensor.distance * 100
+    return objectDis
+
